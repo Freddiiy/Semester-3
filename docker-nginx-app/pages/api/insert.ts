de14d3d03a9a1dfdb1ec07208b6ea6c0c3a1daa3
Frederik Galler <frederikgaller@live.dk>
2022-01-30 15:54:01 +0100

    added some small stuff

--- docker-nginx-app/pages/api/insert.ts
+++ docker-nginx-app/pages/api/insert.ts
@@ -5,6 +5,7 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-    insertText();
-    res.status(200).send("Ok");
+    console.log(req.body);
+    insertText(req.body);
+    res.status(200).redirect("/");
 }
--- docker-nginx-app/pages/index.tsx
+++ docker-nginx-app/pages/index.tsx
@@ -7,19 +7,74 @@ import { getText } from "../util/dbconnect"
 import axios from "axios"
 
 const Home: NextPage = () => {
-  const [text, setText] = useState("")
+  return (
+    <>
+      <List />
+      <InsertForm />
+    </>
+  )
+}
+
+function InsertForm() {
+  const [formData, setFormData] = useState({
+    text: "",
+  })
+
+  function handleChange(event: any) {
+    const value = event.target.value
+    setFormData({
+      ...formData,
+      [event.target.name]: value,
+    })
+  }
+
+  function handleSubmit(event: any) {
+    event.preventDefault()
+    const form = event.currentTarget
+
+    const data = {
+      text: formData.text,
+    }
+
+    axios.post("/api/insert", data)
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="text">Insert text</label>
+      <input
+        type="text"
+        name="text"
+        id="text"
+        value={formData.text}
+        onChange={handleChange}
+      ></input>
+      <input type="submit" value="send"></input>
+    </form>
+  )
+}
+
+function List() {
   const [loaded, setLoaded] = useState(false)
+  const [items, setItems] = useState([])
 
   useEffect(() => {
-    axios.get("/api/db").then((res) => {
-      setText(res.data)
-      setLoaded(true)
-    })
+    axios
+      .get("/api/db")
+      .then((res) => {
+        setItems(res.data)
+      })
+      .then(() => {
+        setLoaded(true)
+      })
   }, [])
 
   return (
     <>
-      <h1>from db: {loaded ? text : "not loaded"}</h1>
+      <ul>
+        {items.map((item) => (
+          <li key={item}>cock</li>
+        ))}
+      </ul>
     </>
   )
 }
