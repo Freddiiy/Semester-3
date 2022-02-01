f3366afa460f86a9885fa95fa1932cd79fceae16
Frederik Galler <frederikgaller@live.dk>
2022-02-01 11:46:13 +0100

    added bootstrap bc css sucks

--- docker-nginx-app/pages/index.tsx
+++ docker-nginx-app/pages/index.tsx
@@ -1,99 +1,112 @@
-import type { NextPage } from "next"
-import { useCallback, useEffect, useRef, useState } from "react"
-import axios from "axios"
-import { text } from "stream/consumers"
+import type { NextPage } from "next";
+import { useCallback, useEffect, useState } from "react";
+import axios from "axios";
+import {
+  Container,
+  ListGroup,
+  ListGroupItem,
+  Form,
+  Button,
+  Row,
+  InputGroup,
+} from "react-bootstrap";
 
 const Home: NextPage = () => {
   return (
     <>
       <InsertForm />
     </>
-  )
+  );
+};
+
+interface Items {
+  id: number;
+  text: string;
 }
 
 function InsertForm() {
   const [formData, setFormData] = useState({
     text: "",
-  })
+  });
 
-  interface Items {
-    id: number
-    text: string
-  }
-
-  const [send, setSend] = useState("")
-  const [items, setItems] = useState<Items[]>([])
-  const listItems = useRef<Items>()
+  const [send, setSend] = useState("");
+  const [items, setItems] = useState<Items[]>([]);
 
   function handleChange(event: any) {
-    const value = event.target.value
+    const value = event.target.value;
     setFormData({
       ...formData,
       [event.target.name]: value,
-    })
+    });
   }
 
   const handleSubmit = useCallback(
     (event: any) => {
-      event.preventDefault()
+      event.preventDefault();
 
       const data = {
         text: formData.text,
-      }
-      if (formData.text == "") return
+      };
+      if (formData.text == "") return;
 
       axios
         .post("/api/insert", data)
         .then((res) => {
           if (res.status == 200) {
-            setSend("sendt")
+            setSend("sendt");
             const next: Items[] = [
               ...items,
               { id: event.target.text, text: formData.text },
-            ]
-            setItems(next)
-            setFormData({ text: "" })
+            ];
+            setItems(next);
+            setFormData({ text: "" });
           }
         })
         .catch(() => {
-          setSend("Ikke sendt")
-        })
+          setSend("Ikke sendt");
+        });
     },
     [formData, items]
-  )
+  );
 
   async function fetchData() {
     axios.get("/api/read").then((res) => {
-      setItems(res.data)
-    })
+      setItems(res.data);
+    });
   }
 
   useEffect(() => {
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   return (
     <>
-      <ul>
-        {items.map((item: Items) => (
-          <li key={item.id}>{item.text}</li>
-        ))}
-      </ul>
-
-      <p>{send}</p>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="text">Insert text</label>
-        <input
-          type="text"
-          name="text"
-          id="text"
-          value={formData.text}
-          onChange={handleChange}
-        ></input>
-        <input type="submit" value="send"></input>
-      </form>
+      <Container className="p-2">
+        <div className="scroll-box">
+          <ListGroup>
+            {items.map((item: Items) => (
+              <ListGroupItem key={item.id}>{item.text}</ListGroupItem>
+            ))}
+          </ListGroup>
+        </div>
+      </Container>
+      <Container>
+        <Form onSubmit={handleSubmit}>
+          <InputGroup>
+            <InputGroup.Text>Insert text</InputGroup.Text>
+            <Form.Control
+              name="text"
+              type="text"
+              placeholder="Skriv todo"
+              value={formData.text}
+              onChange={handleChange}
+            />
+            <Button type="submit">Send</Button>
+          </InputGroup>
+        </Form>
+      </Container>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
