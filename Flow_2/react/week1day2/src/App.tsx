aec7a8adeaf124506c7766182c22345eb938b118
Freddiiy <frederikgaller@live.dk>
2022-03-30 08:52:38 +0200

    nextjs app init

--- Flow_2/react/week1day2/src/App.tsx
+++ Flow_2/react/week1day2/src/App.tsx
@@ -2,40 +2,14 @@ import { useContext, useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { act } from "react-dom/test-utils";
+import { Counter } from "./components/Counter";
+import { Joke } from "./components/Joke";
 
 function App() {
-  return <Counter initCount={9} />;
-}
-
-function Counter({ initCount = 5 }: { initCount?: number }) {
-  const countKey = "countKey";
-  const [count, setCount] = useState(initCount);
-
-  let count2: number = 20;
-
-  useEffect(() => {
-    localStorage.getItem(countKey);
-    const countExist = localStorage.getItem(countKey);
-    setCount(countExist ? parseInt(countExist) : 200);
-  }, []);
-
-  function handleIncrease() {
-    const newCount = count + 1;
-    localStorage.setItem(countKey, String(newCount));
-    setCount(newCount);
-  }
-
-  function handleDecrease() {
-    const newCount = count - 1;
-    localStorage.setItem(countKey, String(newCount));
-    setCount(newCount);
-  }
-
   return (
     <>
-      <h1>{count}</h1>
-      <button onClick={handleIncrease}>+</button>
-      <button onClick={handleDecrease}>-</button>
+      <Counter initCount={30} />
+      <Joke />
     </>
   );
 }
